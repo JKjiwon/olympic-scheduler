61c7ceb25462355178a76a80a0269306210aebe9
kimjiwon <jwkim.oa@gmail.com>
2021-07-30 13:28:54 +0900

    first commit

---
+++ src/main/java/kr/olympicschedule/onair/OnairApplication.java
@@ -0,0 +1,13 @@
+package kr.olympicschedule.onair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnairApplication.class, args);
+    }
+
+}
